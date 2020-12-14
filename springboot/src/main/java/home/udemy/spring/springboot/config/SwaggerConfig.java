26ad50b9859f90d551454ede97c22f3aa1f769a9
Devil <vivek.nakade@gmail.com>
2020-12-14 23:47:36 +0530

    Adding swagger and actuator configuration

---
+++ springboot/src/main/java/home/udemy/spring/springboot/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package home.udemy.spring.springboot.config;
+
+import java.util.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	public static final Contact DEFAULT_CONTACT = new Contact(
+			"Vivek Nakade","","");
+	
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+			"Spring Boot API", "API Description", "1.0",
+			"urn:tos", DEFAULT_CONTACT, 
+			"", "", Arrays.asList());
+
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
+			new HashSet<String>(Arrays.asList("application/json",
+					"application/xml"));
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+	}
+}
--- springboot/src/test/java/home/udemy/spring/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/home/udemy/spring/springboot/SpringbootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
